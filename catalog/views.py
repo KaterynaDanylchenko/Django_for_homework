4f73ad06ff5a48633d8dd07e7c42596b61f6b33c
Kateryna_Danylchenko <ekaterynadanilchenko@ukr.net>
2022-10-19 16:43:58 +0300

    Improving_our_view

--- catalog/views.py
+++ catalog/views.py
@@ -4,8 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Choice, Question
-
-
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -14,7 +13,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -28,5 +29,3 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-
-
